b101d9fa964a14b71fe37e7b042a24bb655d4f99
Joedson <joedsoncte@gmail.com>
2024-07-17 00:27:13 -0300

    Aluno - Instalado biblioteca react-native-uuid que permite a geração de uuid dos beacons, que é basicamente o ID de um ibeacon

--- aluno/app/src/pages/Teste/index.js
+++ aluno/app/src/pages/Teste/index.js
@@ -1,23 +1,22 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Text, View, Alert, Platform, PermissionsAndroid, Button } from 'react-native';
 import BeaconBroadcast from '@lovemh9395/react-native-ibeacon-simulator';
+import uuid from 'react-native-uuid';
 import styles from "./styles";
 
-const uuid = 'fda50693-a4e2-4fb1-afcf-c6eb07647825';
-const identifier = 'MyBeacon';
-const major = 1;
-const minor = 1;
+const BeaconBackgroundScanScreen = () => {
+  const [isBroadcasting, setIsBroadcasting] = useState(false);
+  const [uuidValue, setUuidValue] = useState(uuid.v4());
+  //const uuidValue = 'fda50693-a4e2-4fb1-afcf-c6eb07647825';
+  const identifier = 'MyBeacon';
+  const major = 1;
+  const minor = 1;
 
-class BeaconBackgroundScanScreen extends Component {
-  state = {
-    isBroadcasting: false,
-  };
+  useEffect(() => {
+    checkAndRequestPermissions();
+  }, []);
 
-  async componentDidMount() {
-    await this.checkAndRequestPermissions();
-  }
-
-  checkAndRequestPermissions = async () => {
+  const checkAndRequestPermissions = async () => {
     try {
       if (Platform.Version >= 31) {
         const granted = await PermissionsAndroid.requestMultiple([
@@ -45,17 +44,17 @@ class BeaconBackgroundScanScreen extends Component {
         Alert.alert('O aplicativo não suporta versões do Android anteriores à 6.0 (Marshmallow).');
         return false;
       }
-      this.startBeaconBroadcasting();
+      startBeaconBroadcasting();
     } catch (error) {
       console.error('Erro ao solicitar permissões:', error);
     }
   };
 
-  startBeaconBroadcasting = () => {
+  const startBeaconBroadcasting = () => {
     BeaconBroadcast.checkTransmissionSupported()
       .then(() => {
-        BeaconBroadcast.startAdvertisingBeaconWithString(uuid, identifier, major, minor);
-        this.setState({ isBroadcasting: true });
+        BeaconBroadcast.startAdvertisingBeaconWithString(uuidValue, identifier, major, minor);
+        setIsBroadcasting(true);
         console.log('Beacon broadcasting started successfully.');
       })
       .catch((error) => {
@@ -63,24 +62,29 @@ class BeaconBackgroundScanScreen extends Component {
       });
   };
 
-  stopBeaconBroadcasting = () => {
+  const stopBeaconBroadcasting = () => {
     BeaconBroadcast.stopAdvertisingBeacon();
-    this.setState({ isBroadcasting: false });
+    setIsBroadcasting(false);
     console.log('Beacon broadcasting stopped.');
   };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.header}>Detecção de Beacons</Text>
-        {this.state.isBroadcasting ? (
-          <Button title="Parar Simulação de Beacon" onPress={this.stopBeaconBroadcasting} />
-        ) : (
-          <Button title="Iniciar Simulação de Beacon" onPress={this.startBeaconBroadcasting} />
-        )}
-      </View>
-    );
-  }
-}
+  const generateUUID = () => {
+    const newUuid = uuid.v4(); // Gera um UUID v4
+    setUuidValue(newUuid);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Detecção de Beacons</Text>
+      {isBroadcasting ? (
+        <Button title="Parar Simulação de Beacon" onPress={stopBeaconBroadcasting} />
+      ) : (
+        <Button title="Iniciar Simulação de Beacon" onPress={startBeaconBroadcasting} />
+      )}
+      <Text>UUID atual: {uuidValue}</Text>
+      <Button title="Gerar UUID" onPress={generateUUID} />
+    </View>
+  );
+};
 
 export default BeaconBackgroundScanScreen;
