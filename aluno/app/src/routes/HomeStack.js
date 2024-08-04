35c152ac7a3e6e52d220970e7afed341bd8b50ed
Joedson <joedsoncte@gmail.com>
2024-08-04 14:17:39 -0300

    Aluno -> Removido outras telas de testes que novamente esqueci de remover

--- aluno/app/src/routes/HomeStack.js
+++ aluno/app/src/routes/HomeStack.js
@@ -1,11 +1,8 @@
 import React from "react";
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Home from '../pages/Home';
 import AlunoEdita from '../pages/Aluno/AlunoEdita';
-
-import Teste from "../pages/Teste";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,8 +11,6 @@ const Routes = () => {
         <Stack.Navigator initialRouteName="Home">
             <Stack.Screen options={{ headerShown: false }} name="Home" component={Home}/>
             <Stack.Screen options={{ headerShown: false }} name="AlunoEdita" component={AlunoEdita}/>
-            
-            <Stack.Screen options={{ headerShown: false }} name="Teste" component={Teste}/>
         </Stack.Navigator>
     )
 }
